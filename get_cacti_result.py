183ceec5f38add99c81349e5fca51277757378ae
zmao@ubuntu-vbox <maoze365@gmail.com>
2013-11-19 23:20:40 -0800

    first ok

--- get_cacti_result.py
+++ get_cacti_result.py
@@ -1,6 +1,12 @@
+#!/usr/usc/python/2.6.1/bin/python
 import urllib  
 import urllib2 
 import re
+import sys
+import os
+import glob
+from optparse import OptionParser
+
 def get_normal(cache_size,line_size,assoc,nrbanks,technode):
 	url = 'http://quid.hpl.hp.com:9081/cacti/index.y'  
 	values = {'cache_size' : cache_size,  
@@ -110,15 +116,59 @@ def get_sram(cache_size,read_ports,write_ports,nr_bits_read_out):
 		nr_bits_read_out=str(nr_int)
 		print "recursion"
 		return get_sram(cache_size,read_ports,write_ports,nr_bits_read_out)
-
+def get_min_int(float_a):
+	int_a=int(float_a)
+	if (float_a-int_a > 0):
+		return int_a+1
+	else:
+		return int_a
 def main():
-	the_result = get_sram('616','7','6','154')
-	print "Sram Access time:", the_result
 
-	the_result= get_detai('16384','32','2','1','1','256','29')
-	print "L1 instruction:", the_result
-	#cache_size, line_size,assoc,read_ports, write_ports, nr_bits_read_out, tagbits 
+	parser = OptionParser()
+	parser.add_option("-a", "--l1inst",dest="l1inst",help="cache_size, line_size,assoc,read_ports, write_ports, nr_bits_read_out")
+	parser.add_option("-b", "--l1data",dest="l1data",help="")
+	parser.add_option("-c", "--l2inst",dest="l2inst",help="")
+	parser.add_option("-d", "--l2data",dest="l2data",help="")
+	parser.add_option("-f", "--sram",dest="sram",help="cache_size, read_ports, write_ports, nr_bits_read_out")
+	(options, args) = parser.parse_args()	
 
+	l1inst_list=options.l1inst.split()
+	l1data_list=options.l1data.split()
+	l2inst_list=options.l2inst.split()
+	l2data_list=options.l2data.split()
+	sram_list=options.sram.split()
+
+	l1inst_access_time=get_detai(l1inst_list[0],l1inst_list[1],l1inst_list[2] ,l1inst_list[3] ,l1inst_list[4],l1inst_list[5],'29') 
+	l1data_access_time=get_detai(l1data_list[0],l1data_list[1],l1data_list[2] ,l1data_list[3] ,l1data_list[4],l1data_list[5],'30') 
+	l2inst_access_time=get_detai(l2inst_list[0],l2inst_list[1],l2inst_list[2] ,l2inst_list[3] ,l2inst_list[4],l2inst_list[5],'28') 
+	if l2data_list[0]=='NULL' :
+		l2data_access_time=l2inst_access_time
+	else:
+		l2data_access_time=get_detai(l2data_list[0],l2data_list[1],l2data_list[2] ,l2data_list[3] ,l2data_list[4],l2data_list[5],'28') 
+	sram_access_time=get_sram(sram_list[0],sram_list[1],sram_list[2] ,sram_list[3]) 
+	
+	print "L1inst:",l1inst_access_time	
+	print "L1data:",l1data_access_time
+	print "L2inst:",l2inst_access_time
+	print "L2data:",l2data_access_time
+	print "RuuRAM:",sram_access_time
+
+	l1inst_float=float(l1inst_access_time)	
+	l1data_float=float(l1data_access_time)
+	l2inst_float=float(l2inst_access_time)
+	l2data_float=float(l2data_access_time)
+	sram_float=float(sram_access_time)
+
+	l1inst_int=get_min_int(l1inst_float/sram_float)
+	l1data_int=get_min_int(l1data_float/sram_float)
+	l2inst_int=get_min_int(l2inst_float/sram_float)
+	l2data_int=get_min_int(l2data_float/sram_float)	
+
+	print "L1inst cycles:",l1inst_int	
+	print "L1data cycles:",l1data_int
+	print "L2inst cycles:",l2inst_int
+	print "L2data cycles:",l2data_int
+	print "RuuRAM cycles:",'1'
 
 if __name__ == '__main__':
     main()
