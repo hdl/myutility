e5f6f7ad9b5438fc833f8969c7b60e7ffb259222
zmao@nunki <zemao@usc.edu>
2013-11-22 23:29:28 -0800

    fix bug

--- para_pick_up.py
+++ para_pick_up.py
@@ -6,10 +6,13 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-f", "--file",dest="filename",help="specify the file you want to process, *.mips means all the mips file in current dirctory")
-parser.add_option("-p", "--para",dest="parameter",help="specify the parameter you want to get")
+parser.add_option("-p", "--para",dest="parameter",help="specify the parameter you want to get. If this is not specified, it's MIPS art_mips twolf_mips mcf_mips applu_mips")
 (options, args) = parser.parse_args()
 
-parameter_list=options.parameter.split()
+if type(options.parameter)==type(None):
+	parameter_list=("MIPS:", "art_mips", "twolf_mips", "mcf_mips", "applu_mips")
+else:
+	parameter_list=options.parameter.split()
 
 if "*" in options.filename:
 	file_list=glob.glob(options.filename)
@@ -19,9 +22,14 @@ else:
 
 print "dir:",os.path.abspath('.')
 print "file name\t",
-for parameter in parameter_list:
-	print parameter,"\t",
+
+fp=open(file_list[0])
+for line in fp.readlines():
+	for parameter in parameter_list:
+		if parameter in line:
+			print parameter,"\t",
 print " "
+fp.close()
 
 
 
